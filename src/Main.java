88bc3d00b329934b1cabe394d7f5165605c81def
Elena <lenulka.90@mail.ru>
2023-05-04 07:39:31 +0300

    First exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+         // Пишем код для задачи 1
+        var box = 5;
+        System.out.println ("box = " + box);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+         // Пишем код для задачи 2
     }
 }
